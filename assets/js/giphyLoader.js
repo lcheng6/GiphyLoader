d1ac015fd9bc6f4bb2b145c680528967d32a68cc
Liang Cheng <liangcheng6@gmail.com>
2017-03-21 02:42:56 -0400

    initial submittal

--- assets/js/giphyLoader.js
+++ assets/js/giphyLoader.js
@@ -1,83 +1,125 @@
 'use strict'
 var animalsArray = ["dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
-	"bird", "ferret", "sugar glider", "chinchilla", "hedgehog", "hermit crab", 
-	"gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", 
-	"salamander", "frog"];
+	"bird", "ferret", "sugar glider", "chinchilla", "hedgehog", "hermit crab",
+	"gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval",
+	"salamander", "frog"
+];
 
 var giphyFixedSizedArray = [];
 var giphyOriginalArray = [];
 
-var maxResults = 50;
-var resultsPerSearch = 10;
+//The selected Animal for the search
+var selectedAnimal = "";
+var animalSearchOffset = 0;
 
-
-var loadInitialButtons = function() {
-	for (var i=0; i<animalsArray.length; i++) {
-		var animal = animalsArray[i];
-		var button = $('<button>')
-			.addClass('btn').addClass('btn-info')
-			.text(animal)
-			.click(function() {
-				console.log($(this).text())
-				doGiphySearch(animal);
-			})
-		$("#animalButtons").append(button);
-	}
-}
 
 var submitBtnClick = function(event) {
 	event.preventDefault();
 	var animal = $("#animal-input").val();
 	if (animal) {
 		console.log('new animal: ' + animal);
+		selectedAnimal = animal;
+		('.image-grid').empty()
 		var button = $('<button>')
 			.addClass('btn').addClass('btn-info')
-				.text(animal)
-				.click(function() {
-					console.log($(this).text())
-				})
-			$('#animalButtons').append(button);
+			.text(animal)
+			.click(function() {
+				console.log($(this).text())
+			})
+		$('#animalButtons').append(button);
 	}
-	
+
 }
 
-var doGiphySearch = function (animal, offset) {
-	//exmpale giphy search: https://api.giphy.com/v1/gifs/search?q=bison&api_key=dc6zaTOxFJmzC&limit=10
-	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +animal+ "&api_key=dc6zaTOxFJmzC&limit=10&offset="+offset;
-	$.ajax({
-		url:queryURL,
-		method:"Get"
-	})
-	.done(function(response) {
-		var results = response.data
-		for (var i=0; i<results.length; i++) {
-			giphyFixedSizedArray.push(results[i].images.fixed_height.url);
-			giphyOriginalArray.push(results[i].images.original.url);
+var displayGiphyResults = function(animal) {
+	var html = '',
+		imageNum;
+
+	for (var i = 0; i < giphyFixedSizedArray.length; i++) {
+		imageNum = animalSearchOffset + i;
+		html += '<article class="image__cell is-collapsed">' +
+			'<div class="image--basic">' +
+			'<a href="#expand-jump-' + i + '">' +
+			'<img id="expand-jump-' + i + '" class="basic__img" src="' + giphyFixedSizedArray[i] + '" alt="Fashion ' + imageNum + '" />' +
+			'</a>' +
+			'<div class="arrow--up"></div>' +
+			'</div>' +
+			'<div class="image--expand">' +
+			'<a href="#close-jump-' + i + '" class="expand__close"></a>' +
+			'<img class="image--large" src="' + giphyOriginalArray[i] + '" alt="Animal ' + imageNum + '" />' +
+			'</div>' +
+			'</article>';
+	}
+	$('.image-grid').append(html);
+
+	var $cell = $('.image__cell');
+
+	$cell.find('.image--basic').click(function() {
+		var $thisCell = $(this).closest('.image__cell');
+
+		if ($thisCell.hasClass('is-collapsed')) {
+			$cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed');
+			$thisCell.removeClass('is-collapsed').addClass('is-expanded');
+		} else {
+			$thisCell.removeClass('is-expanded').addClass('is-collapsed');
 		}
-		displayGiphyResults();
-	})
-}
-var displayGiphyResults = function () {
-	
-	for (var i=0; i<giphyResultArray.length; i++) {
-		var div = $('<div/>')
-			.append(
-				$('<img/>')
-				.attr('src', giphyResultArray[i]
-				)
-			);
-		$('#animals').append(div);
-	}
+	});
+
+	$cell.find('.expand__close').click(function() {
+
+		var $thisCell = $(this).closest('.image__cell');
+
+		$thisCell.removeClass('is-expanded').addClass('is-collapsed');
+	});
 }
 
-var clearExistingAnimalsGifs = function() {
-
+var doGiphySearch = function(animal) {
+	//exmpale giphy search: https://api.giphy.com/v1/gifs/search?q=bison&api_key=dc6zaTOxFJmzC&limit=10
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10&offset=" + animalSearchOffset.toString();
+	$.ajax({
+			url: queryURL,
+			method: "Get"
+		})
+		.done(function(response) {
+			var results = response.data
+			for (var i = 0; i < results.length; i++) {
+				giphyFixedSizedArray.push(results[i].images.fixed_height.url);
+				giphyOriginalArray.push(results[i].images.original.url);
+			}
+			animalSearchOffset = animalSearchOffset + results.length;
+			displayGiphyResults();
+		})
 }
+
+
+
 var animalButtonClicked = function() {
+	$('.image-grid').empty();
+	animalSearchOffset = 0;
+	$('#progressloader').hide();
+	selectedAnimal = $(this).text();
+	doGiphySearch(selectedAnimal);
+}
 
+var loadInitialButtons = function() {
+	for (var i = 0; i < animalsArray.length; i++) {
+		var animal = animalsArray[i];
+		var button = $('<button>')
+			.addClass('btn').addClass('btn-info')
+			.text(animal)
+			.click(animalButtonClicked)
+		$("#animalButtons").append(button);
+	}
 }
 
 $(document).ready(function() {
 	loadInitialButtons();
 	$("#addAnimal").on("click", submitBtnClick);
+});
+
+
+$(window).scroll(function() {
+	if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+		console.log("bottom!");
+	}
 });
--- assets/js/image-grid.js
+++ assets/js/image-grid.js
@@ -3,53 +3,7 @@
 
 // dynamically create loads of image cells
 
-var testGifs = 
-    ["https://media4.giphy.com/media/gw3MYmhxEv8T52ow/200.gif",
-     "https://media1.giphy.com/media/iwqJk8sFdwcxy/200.gif",
-    "https://media4.giphy.com/media/4N4JBFo45PVPa/200.gif",
-    "https://media4.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif",
-    "https://media2.giphy.com/media/1mht8tODXy0OA/200.gif",
-    "https://media4.giphy.com/media/J0UgtZkOjXOeY/200.gif",
-    "https://media1.giphy.com/media/vGD2uBftnl0Q0/200.gif",
-    "https://media2.giphy.com/media/I0wazlBIZhvwY/200.gif",
-    "https://media4.giphy.com/media/FVgxUIjL8neWk/200.gif",
-    "https://media4.giphy.com/media/26xBwmgBDUs9G2uFG/200.gif",
-    "https://media4.giphy.com/media/gw3MYmhxEv8T52ow/200.gif",
-    "https://media1.giphy.com/media/iwqJk8sFdwcxy/200.gif",
-    "https://media4.giphy.com/media/4N4JBFo45PVPa/200.gif",
-    "https://media4.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif",
-    "https://media2.giphy.com/media/1mht8tODXy0OA/200.gif",
-    "https://media4.giphy.com/media/J0UgtZkOjXOeY/200.gif",
-    "https://media1.giphy.com/media/vGD2uBftnl0Q0/200.gif",
-    "https://media2.giphy.com/media/I0wazlBIZhvwY/200.gif",
-    "https://media4.giphy.com/media/FVgxUIjL8neWk/200.gif",
-    "https://media4.giphy.com/media/26xBwmgBDUs9G2uFG/200.gif"];
-function cells(count) {
-  if (typeof count !== 'number' || count > 99) return false;
-  
-  var html = '',
-      imageNum;
-  
-  for (i = 0; i < count; i++) {
-    imageNum = Math.floor(Math.random() * 9) + 1;
-    html += '<article class="image__cell is-collapsed">' +
-	    '<div class="image--basic">' +
-		    '<a href="#expand-jump-'+i+'">' +
-		        '<img id="expand-jump-'+i+'" class="basic__img" src="' + testGifs[imageNum] + '" alt="Fashion '+ imageNum +'" />' +
-	      '</a>' +
-	      '<div class="arrow--up"></div>' +
-	    '</div>' +
-	    '<div class="image--expand">' +
-		    '<a href="#close-jump-'+i+'" class="expand__close"></a>' +
-          '<img class="image--large" src="'+ testGifs[imageNum] +'" alt="Fashion '+ imageNum +'" />' +
-	    '</div>' +
-	  '</article>';
-  }
-  return html;
-}
 
-//apend cells to grid
-$('.image-grid').empty().html(cells(50));
 
 
 //bind click events
